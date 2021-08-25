1d0195899b9b06ad33f41368d0fb3259fa501951
Nicolas <nicolas.pelissier@wiifor.com>
2021-08-25 11:40:43 +0200

    s2844 - Add initial LR1110 support
    Signed-off-by: Nicolas <nicolas.pelissier@wiifor.com>

---
+++ include/dt-bindings/lora/lr1110.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 Wiifor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_LORA_LR1110_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_LORA_LR1110_H_
+
+#define LR1110_TCXO_1V6	0x00
+#define LR1110_TCXO_1V7	0x01
+#define LR1110_TCXO_1V8	0x02
+#define LR1110_TCXO_2V2	0x03
+#define LR1110_TCXO_2V4	0x04
+#define LR1110_TCXO_2V7	0x05
+#define LR1110_TCXO_3V0	0x06
+#define LR1110_TCXO_3V3	0x07
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_LORA_LR1110_H_ */
